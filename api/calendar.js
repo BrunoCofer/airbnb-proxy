98b95a04b2592fb99ecd305d854b0238161c8f9a
Cofer <119969217+BrunoCofer@users.noreply.github.com>
2025-05-02 01:55:00 -0300

    Update calendar.js

--- api/calendar.js
+++ api/calendar.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    const url = "http://www.vrbo.com/icalendar/43c8e2d5349b46bd979334a380a0c896.ics?nonTentative";
+    const url = "https://www.airbnb.com/calendar/ical/786877636617815246.ics?s=4319386076183b6313e63daa79cff5b9";
   
     try {
       const response = await fetch(url);
